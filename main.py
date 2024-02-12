a77bc5c6ad8889a44642a201903f061c67054993
uzoanya <daniel.uzoanya@binghamuni.edu.ng>
2024-02-12 22:27:50 +0100

    comments added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sqlite3
 
 window = tk.Tk()
+# App title
 window.title("My Finance Tracker App")
 window.geometry("450x450")
 window.configure(bg="#ecf0f1", borderwidth="30")
@@ -15,6 +16,7 @@ combo_category = ttk.Combobox()
 combo_category.grid(row=2, column=1, padx=5, pady=5)
 
 
+# our sqlite3 database which saves the user expenses.
 def create_table():
     conn = sqlite3.connect("finance_tracker.db")
     cursor = conn.cursor()
@@ -39,6 +41,8 @@ def add_expense(description, amount, category):
     conn.close()
 
 
+# connects to a SQLite database, retrieves expense data grouped by category,
+# and displays it as pie chart using matplotlib.
 def show_pie_chart():
     conn = sqlite3.connect("finance_tracker.db")
     cursor = conn.cursor()
@@ -85,6 +89,9 @@ def save_expense():
         combo_category.set("")
 
 
+# function  which sets up a GUI window with labels, entry fields,
+# dropdown menu, and buttons for saving expenses and displaying them using pie or bar charts,
+# then run the main function.
 def main():
     create_table()
 
@@ -95,13 +102,13 @@ def main():
     entry_amount.grid(row=1, column=2, padx=5, pady=5)
 
     ttk.Label(window, text="Category:", foreground="#2c3e50", borderwidth="30").grid(row=2, column=0, padx=5, pady=5)
-    categories = ["Groceries", "Entertainment", "Utilities", "clothing", "others"]
+    categories = ["Groceries", "Entertainment", "Utilities", "Clothing", "Others"]
     combo_category.config(values=categories)
     combo_category.grid(row=2, column=2, padx=5, pady=5)
 
     ttk.Button(window, text="Save Expense", command=save_expense).grid(row=11, column=2, columnspan=2, pady=10)
-    ttk.Button(window, text="Expense With Pie Chart", command=show_pie_chart).grid(row=12, column=1, pady=5)
-    ttk.Button(window, text="Expense With Bar Chart", command=show_bar_chart).grid(row=13, column=1, pady=5)
+    ttk.Button(window, text="View Expenses With Pie Chart", command=show_pie_chart).grid(row=12, column=1, pady=5)
+    ttk.Button(window, text="View Expenses With Bar Chart", command=show_bar_chart).grid(row=13, column=1, pady=5)
 
     window.mainloop()
 
